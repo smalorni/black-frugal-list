8f1e4c7f416e66c671f151cfb477f5dab51bb985
Samantha Malorni <samantha.ann.malorni@gmail.com>
2022-10-30 20:31:57 -0400

    created start-up files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BlackFrugalList from './frugalList';
+
+/* Export from frugalList */
+
+function App() {
+  return (
+    <div>
+     <BlackFrugalList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frugalList.js
@@ -0,0 +1,11 @@
+/* Create our main list */
+
+const BlackFrugalList = () => {
+    return (
+        <h1>Test</h1>
+    )
+}
+
+//export default BlackFrugalList
+export default BlackFrugalList
+
